260732d71590fc3ad504e0520ec466df4495e2d4
TOMOTHON <tomothonnn@gmail.com>
2025-01-18 20:22:03 -0500

    Prototype 2

--- MainGame.gd
+++ MainGame.gd
@@ -1,23 +1,28 @@
 extends Node2D
 
-var height: int = 0
+var treeheight: float = 0 
+var treeheightstart: float = 0
+var screenheight: float = 0
+var BlackFadeAlpha: float = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$HeightCounter/Timer.start()
-	$Button.text = ("amogus")
+	treeheightstart = %FarBackgroundUnfinished.texture.get_height()
+	$BlackFadeBox/BlackFadeAnimationPlayer.play("fade_to_normal")
+	get_window().position.y = 20 #because the stupid window bar always starts off screen
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	treeheight+=delta
+	%HeightCounter.text = "Height: " + str(round_place(treeheight,2))
+	%FarBackgroundUnfinished.position.y += delta*50
 	pass
-	#print(height)
-	#print($"HeightCounter".text)
+
+func round_place(num,places):
+	return (round(num*pow(10,places))/pow(10,places))
 
 
-func _on_timer_timeout():
-	print("peen") # Replace with function body.
-	height += 1
-	$HeightCounter.text = "Height: " + str(height)
+
 
