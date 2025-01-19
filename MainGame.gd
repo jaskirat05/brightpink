d985175fd52cdbebefb53c8bc1e40dd13b9f5f57
TOMOTHON <tomothonnn@gmail.com>
2025-01-18 21:53:56 -0500

    Added Assets and fixed camera movement

--- MainGame.gd
+++ MainGame.gd
@@ -3,26 +3,33 @@ extends Node2D
 var treeheight: float = 0 
 var treeheightstart: float = 0
 var screenheight: float = 0
-var BlackFadeAlpha: float = 0
+var startpan: float = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	treeheightstart = %FarBackgroundUnfinished.texture.get_height()
-	$BlackFadeBox/BlackFadeAnimationPlayer.play("fade_to_normal")
-	get_window().position.y = 20 #because the stupid window bar always starts off screen
+	%Camera.offset.y = 200
+	
+	%BlackFadeAnimationPlayer.play("fade_to_normal")
+	get_window().position.y = 50 #because the stupid window bar always starts off screen
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	treeheight+=delta
-	%HeightCounter.text = "Height: " + str(round_place(treeheight,2))
-	%FarBackgroundUnfinished.position.y += delta*50
+	if(%Camera.offset.y > -480):
+		%Camera.offset.y = lerp(%Camera.offset.y, -500.0, 0.01)
+	
+	else:
+		treeheight+=delta
+		%HeightCounter.text = "Height: " + str(round_place(treeheight,2))
+		%Tree.position.y -= delta*50
 	pass
 
 func round_place(num,places):
 	return (round(num*pow(10,places))/pow(10,places))
 
-
+func spawn_sun() -> void:
+	pass
+	
 
 
