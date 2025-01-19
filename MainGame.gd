f59368306fc9b312a1ade59c553511eda451c368
jas <80989797+jaskirat05@users.noreply.github.com>
2025-01-19 15:29:28 +0530

    updated

--- MainGame.gd
+++ MainGame.gd
@@ -6,12 +6,13 @@ var CameraStartOffset: float = 100
 
 var screenheight: float = 0
 var startpan: float = 0
+@onready var tree = $Tree
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	%Camera.offset.y = CameraStartOffset
-
+	
 	%BlackFadeAnimationPlayer.play("fade_to_normal")
 	get_window().position.y = 50 #because the stupid window bar always starts off screen
 
@@ -19,6 +20,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	treeheight+=delta
+	tree.update_difficulty(treeheight)
 	%Camera.offset.y = lerp(CameraStartOffset,-650.0, ease(treeheight*0.5,-2.5))
 	
 	if(%Camera.offset.y <= -650):
@@ -34,3 +36,5 @@ func round_place(num,places):
 func _on_collectible_input_event(viewport, event, shape_idx):
 	
 	pass # Replace with function body.
+	
+
