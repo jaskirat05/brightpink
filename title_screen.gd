06b0ce06f7edd839c571a9b4e8803399d6c907cb
TOMOTHON <tomothonnn@gmail.com>
2025-01-19 12:11:06 -0500

    Basic Game Changes

---
+++ title_screen.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var startanimation: float = 0
+var titleanimed: bool = true
+var startanimed: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Title.visible = false
+	$Title2.visible = false
+	
+	$"UI Layer/FadeTransitions/AnimationPlayer".play("fade_to_normal")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	startanimation += delta
+	
+	if(startanimation > 3 && titleanimed == true):
+		titleanimed = false
+		$"Title/AnimationPlayer".play("fade_in")
+		$Title.visible = true
+
+
+	
+	if(startanimation >5 && startanimed == true):
+		startanimed = false
+		$"Title2/AnimationPlayer".play("fade_to_normal")
+		$Title2.visible = true
+
+		
+
+func _on_title_2_mouse_entered():
+	print("whoops")
+	$"Title2".add_theme_constant_override("outline_size",20)
+	pass # Replace with function body.
+
+func _on_title_2_mouse_exited():
+	$"Title2".remove_theme_constant_override("outline_size")
+	pass # Replace with function body.
