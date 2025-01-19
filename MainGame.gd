17ed84f79f406792733b7f61c4decb8cc8c01f40
TOMOTHON <tomothonnn@gmail.com>
2025-01-19 12:11:06 -0500

    Re-add Title Scene? (Please don't break)

--- MainGame.gd
+++ MainGame.gd
@@ -1,31 +1,34 @@
 extends Node2D
 
-var treeheight: float = 0 
+#for keeping track of time since start of game
+var delta_time: float = 0
+#Measures height of tree for score purposes 
+var tree_height: float = 0 
 
-var CameraStartOffset: float = 100
+var camera_offset_start: float = 100
 
 var screenheight: float = 0
 var startpan: float = 0
 @onready var tree = $Tree
+var sun_speed 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%Camera.offset.y = CameraStartOffset
-	
-	%BlackFadeAnimationPlayer.play("fade_to_normal")
+	%Camera.offset.y = camera_offset_start
+
+	$"UI Layer/FadeTransitions/AnimationPlayer".play("fade_to_normal")
 	get_window().position.y = 50 #because the stupid window bar always starts off screen
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	treeheight+=delta
-	tree.update_difficulty(treeheight)
-	%Camera.offset.y = lerp(CameraStartOffset,-650.0, ease(treeheight*0.5,-2.5))
+	tree_height+=delta
+	%Camera.offset.y = lerp(camera_offset_start,-650.0, ease(tree_height*0.5,-2.5))
 	
 	if(%Camera.offset.y <= -650):
 
-		%HeightCounter.text = "Height: " + str(round_place(treeheight,2))
+		%HeightCounter.text = "Height: " + str(round_place(tree_height,2))
 		%Tree.position.y -= delta*50
 	pass
 
